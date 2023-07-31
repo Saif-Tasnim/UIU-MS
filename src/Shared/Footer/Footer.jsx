a17327e6de64e36e6000f4c91fc68d8e9b42053a
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-07-31 13:27:56 +0600

    header footer done

---
+++ src/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-black w-full flex justify-center items-center h-14'>
+            <p className='text-white'> All rights reserved to &copy; <span className='text-[#F06517] font-bold'>Team Halogen</span> </p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="navbar bg-[#F06517] pb-3 z-10">
+            <div className="navbar-start font-extrabold text-xl">
+                <Link className="navbar-item">United International University </Link>
+            </div>
+            <div className="navbar-center">
+                <Link className="navbar-item text-white">Home </Link>
+                <Link className="navbar-item text-white">About</Link>
+                <Link className="navbar-item text-white">Academics</Link>
+                <Link className="navbar-item text-white">Research</Link>
+                <Link className="navbar-item text-white"> Contacts </Link>
+            </div>
+            <div className="navbar-end">
+                <Link className="navbar-item">
+                <button className="btn btn-outline-warning text-white">Log In</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
